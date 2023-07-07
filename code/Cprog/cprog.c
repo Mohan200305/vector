0a355ae7bc730accd1140b77d5a3ea278875afc2
Mohan200305 <127096881+Mohan200305@users.noreply.github.com>
2023-07-07 11:15:48 +0530

    add files via upload

--- code/Cprog/cprog.c
+++ code/Cprog/cprog.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-#include "liv.h"
+#include<math.h> 
+#include "liv.h"   //include user defined library that contains required functions
 
 int main()
 {
     double **e1,**e2,**q1,**q2,**q,**X;  
      double k=11;
-     X=matX(0,0);                               //matrix for the point vector X
+     X=loadtxt("X.dat",2,1);                     //matrix for the point vector X
      e1=loadtxt("e1.dat",3,1);                  //reading the values of matrix e1 from the dat file
     print(e1,3,1);   
     e2=scalarProduct(k,e1);                   //multiplying the scalar value with the matrix e1
@@ -18,7 +18,6 @@ int main()
     print(q2,3,3);   
     q=matmul(q2,e2,3,1,1);                    //matrix multiplication of q2 and e2 which produces the resultant matrix q
     print(q,3,1);
-    save_X(X,2,1);                           //saving the values of X in dat file
     save_q(q,3,1);                           //saving the values of q in dat file which is the resultant matrix 
     return 0;
 }
